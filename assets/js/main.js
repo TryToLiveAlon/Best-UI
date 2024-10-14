82f9bd2e1a71d397f13ba7bbc2b807f673acfa96
TryToLiveAlon_owner <156423343+TryToLiveAlon@users.noreply.github.com>
2024-10-14 21:12:21 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,3 +36,10 @@ gsap.from('.home__title', {opacity: 0, duration: 1, delay:1.6, y: 30})
 gsap.from('.home__description', {opacity: 0, duration: 1, delay:1.8, y: 30})
 gsap.from('.home__button', {opacity: 0, duration: 1, delay:2.1, y: 30})
 gsap.from('.home__img', {opacity: 0, duration: 1, delay:1.3, y: 30})
+
+// Function to toggle the menu
+function toggleMenu() {
+    const navbar = document.querySelector('.navbar');
+        navbar.classList.toggle('active');
+        }
+        
