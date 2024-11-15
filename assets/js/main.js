67ded8d449440b7f4757ab6104f3db07aa671d0c
TryToLiveAlon_owner <156423343+TryToLiveAlon@users.noreply.github.com>
2024-11-15 15:45:51 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,15 @@
 /*===== MENU SHOW Y HIDDEN =====*/
 const navMenu = document.getElementById('nav-menu'),
-    toggleMenu = document.getElementById('nav-toggle'),
+
     closeMenu = document.getElementById('nav-close')
 
 // SHOW
+
+
+ 
+
+
+
 toggleMenu.addEventListener('click', ()=>{
     navMenu.classList.toggle('show')
 })
@@ -14,32 +20,15 @@ closeMenu.addEventListener('click', ()=>{
 })
 
 /*===== MOUSEMOVE HOME IMG =====*/
-document.addEventListener('mousemove', move);
-function move(e){
-    this.querySelectorAll('.move').forEach(layer =>{
-        const speed = layer.getAttribute('data-speed')
 
-        const x = (window.innerWidth - e.pageX*speed)/120
-        const y = (window.innerHeight - e.pageY*speed)/120
-
-        layer.style.transform = `translateX(${x}px) translateY(${y}px)`
-    })
+function ShowSideBar(){
+  const menu = document.querySelector('.menu-2');
+  menu.style.display="flex"
 }
 
-/*===== GSAP ANIMATION =====*/
-// NAV
-gsap.from('.nav__logo, .nav__toggle', {opacity: 0, duration: 1, delay:2, y: 10})
-gsap.from('.nav__item', {opacity: 0, duration: 1, delay: 2.1, y: 30, stagger: 0.2,})
+function HideSideBar(){
+    const menu = document.querySelector('.menu-2')
+    menu.style.display="none"
 
-// HOME
-gsap.from('.home__title', {opacity: 0, duration: 1, delay:1.6, y: 30})
-gsap.from('.home__description', {opacity: 0, duration: 1, delay:1.8, y: 30})
-gsap.from('.home__button', {opacity: 0, duration: 1, delay:2.1, y: 30})
-gsap.from('.home__img', {opacity: 0, duration: 1, delay:1.3, y: 30})
-
-// Function to toggle the menu
-function toggleMenu() {
-    const navbar = document.querySelector('.navbar');
-        navbar.classList.toggle('active');
-        }
+}
         
